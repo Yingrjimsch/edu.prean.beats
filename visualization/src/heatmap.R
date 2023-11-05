bda8bc82459750ecab39b40f604b3c6e15d1edf8
Fabian Claudio Annaheim <fabiancannaheim@proton.me>
2023-11-05 12:22:08 +0100

    Correlation heatmap

---
+++ visualization/src/heatmap.R
@@ -0,0 +1,32 @@
+# Load the necessary libraries
+library(ggplot2)
+library(reshape2) # For melting the data frame
+
+# Load the dataset using read.csv with the appropriate encoding
+spotify_data <- read.csv("../../spotify-2023.csv", fileEncoding = "ISO-8859-1", stringsAsFactors = FALSE)
+
+# Clean the column names by removing the trailing "_." if present
+names(spotify_data) <- sub("_\\.$", "", names(spotify_data))
+
+# Get the names of all numerical columns that do not have missing values
+numerical_columns <- names(spotify_data)[sapply(spotify_data, is.numeric) & !colSums(is.na(spotify_data))]
+
+# Calculate the correlation matrix
+corr_matrix <- cor(spotify_data[numerical_columns], use = "complete.obs")
+
+# Melt the correlation matrix into a format suitable for ggplot2
+melted_corr_matrix <- melt(corr_matrix)
+
+# Plot the heatmap
+p <- ggplot(data = melted_corr_matrix, aes(Var1, Var2, fill = value)) +
+    geom_tile(color = "white") +
+    scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                         midpoint = 0, limit = c(-1,1), space = "Lab", 
+                         name="Pearson\nCorrelation") +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, size = 12, hjust = 1),
+          axis.text.y = element_text(size = 12)) +
+    labs(x = '', y = '', title = 'Correlation Matrix Heatmap') 
+
+# Save the plot
+ggsave("../plots/heatmap_correlation_matrix.png", plot = p, width = 12, height = 10, dpi = 300)
--- visualization/src/histograms.R
+++ visualization/src/histograms.R
@@ -11,7 +11,8 @@ numerical_cols <- names(spotify_data)[sapply(spotify_data, is.numeric)]
 
 # Function to plot and save histogram for a given column
 plot_and_save_histogram <- function(column_name, data) {
-  p <- ggplot(data, aes(x=.data[[column_name]])) +
+  clean_column_name <- sub("_\\.$", "", column_name)
+  p <- ggplot(data, aes(x=.data[[clean_column_name]])) +
     geom_histogram(aes(y=after_stat(density), fill=after_stat(count)), bins=20, color="grey20", alpha=0.8) +
     scale_fill_gradient(low="lightblue", high="steelblue") +
     theme_minimal() +
@@ -22,14 +23,14 @@ plot_and_save_histogram <- function(column_name, data) {
       axis.title = element_text(size=12)
     ) +
     labs(
-      title=paste("Distribution of", column_name),
-      x=column_name,
+      title=paste("Distribution of", clean_column_name),
+      x=clean_column_name,
       y="Density",
       fill="Count"
     )
   
   # Save the plot to a PNG file
-  ggsave(paste0("./plots/histogram_", column_name, ".png"), plot = p, width = 10, height = 6, dpi = 300)
+  ggsave(paste0("../plots/histogram_", clean_column_name, ".png"), plot = p, width = 10, height = 6, dpi = 300)
 }
 
 # Iterate through each numerical column and create the plots
