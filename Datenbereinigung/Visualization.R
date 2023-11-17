d4ecc9aa512b5e5523014ff95344f4d5cba7126c
vonwareb <vonwareb@students.zhaw.ch>
2023-11-18 00:20:24 +0100

    - added useful plots as pdf for explanations of task datacleaning

--- Datenbereinigung/Visualization.R
+++ Datenbereinigung/Visualization.R
@@ -1,8 +1,19 @@
-######################### Laden der Libraries ##########################
+##########################################################################
+
+# Laden der Libraries
+
+
+##########################################################################
+
 library("RColorBrewer")
 
 
-######################### Lesen der RData Datei ##########################
+##########################################################################
+
+#  Lesen der RData Datei 
+
+##########################################################################
+
 
 load("spotify_songs_cleaned_with_trans.RData")
 load("spotify_songs_cleaned_with_trans_optima.RData")
@@ -12,14 +23,29 @@ str(spotify_songs_cleaned_with_trans)
 str(spotify_songs_cleaned_with_trans_optima)
 str(spotify_songs_cleaned_without_trans)
 
+##########################################################################
+
+#  Methode zum generieren der Plots 
+
+##########################################################################
+
+
 # definieren der Farbe der Balken im Balkendiagramm
 #color <- brewer.pal(11, "Spectral") 
 color <- brewer.pal(11, "PRGn")
 #color <- brewer.pal(10, "Paired")
 
-
+# Erstellen von Plots je nach Prediktor (numerisch oder kategoriell) und speichern als png
 create_plots <- function(dataframe, dependent_variable) {
   num_predictors <- ncol(dataframe)
+  
+  folder_name <- deparse(substitute(dataframe))
+  
+  if (!dir.exists(folder_name)) {
+    
+    dir.create(folder_name)
+    
+  }
   
   for (i in 1:num_predictors) {
     predictor <- names(dataframe)[i]
@@ -27,6 +53,15 @@ create_plots <- function(dataframe, dependent_variable) {
     if (predictor == dependent_variable) {
       next
     }
+    
+    # plots werden in Folder für Dataset abgelegt
+    plot_type <- ifelse(is.factor(dataframe[[predictor]]), "kategoriell", "numerisch")
+    
+    filename <- paste0(folder_name, "/", predictor, "_", plot_type, ".pdf")
+    
+    pdf(file=filename)
+    
+    
     # Plots für kategorielle Prädiktoren
     if (is.factor(dataframe[[predictor]])) {
       
@@ -46,7 +81,7 @@ create_plots <- function(dataframe, dependent_variable) {
       hist(dataframe[[predictor]], main=paste("Histogramm: ", predictor), col = color, xlab=predictor)
       
       # Dichteplot
-      plot(density(na.omit(dataframe[[predictor]])), main=paste("Dichteplot für", predictor), col = "red", xlab=predictor)
+      plot(density(na.omit(dataframe[[predictor]])), main=paste("Dichteplot: ", predictor), col = "red", xlab=predictor)
       
       # QQ-Plot
       qqnorm(dataframe[[predictor]], main=paste("QQ-Plot: ", predictor))
@@ -67,6 +102,7 @@ create_plots <- function(dataframe, dependent_variable) {
            pch = 19)
       abline(h = 0, col = "red")
     }
+    dev.off()
   }
 }
 
@@ -74,4 +110,25 @@ create_plots <- function(dataframe, dependent_variable) {
 create_plots(spotify_songs_cleaned_with_trans, "streams") #-> auskommentieren, wenn Plots gewünscht!
 create_plots(spotify_songs_cleaned_with_trans_optima, "streams") #-> auskommentieren, wenn Plots gewünscht!
 create_plots(spotify_songs_cleaned_without_trans, "streams") #-> auskommentieren, wenn Plots gewünscht!
+
+
+# kombinierte Histogramms
+library(purrr)
+library(tidyr)
+library(ggplot2)
+
+
+spotify_songs_cleaned_with_trans_optima %>%
+  keep(is.numeric) %>%
+  gather() %>%
+  ggplot(aes(value)) +
+  facet_wrap(~ key, scales = "free") +
+  geom_histogram(aes(fill = ..count..)) +  
+  scale_fill_gradient(low = "lightgreen", high = "darkgreen")  
+
+
+
+
+
+
 
