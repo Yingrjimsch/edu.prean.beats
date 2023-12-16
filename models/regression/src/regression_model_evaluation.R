908f310b7d897d66a702d4a9159cedb1982ada85
Fabian Claudio Annaheim <fabiancannaheim@proton.me>
2023-12-16 09:59:18 +0100

    Regression: backtransformation and observed vs predicted plot

--- models/regression/src/regression_model_evaluation.R
+++ models/regression/src/regression_model_evaluation.R
@@ -1,5 +1,6 @@
 #install.packages("leaps") Install if package not installed
 library(leaps)
+library("ggplot2")
 
 # Define directories
 BASE_DIR <- "./models/regression/"
@@ -9,19 +10,22 @@ DIR_DATA_CLEANING <- "./data/"
 datasets <- list(
     list(
         data = paste0(DIR_DATA_CLEANING, "spotify_songs_cleaned_without_trans.RData"),
-        indices = paste0(BASE_DIR, "artifacts/training_indices_spotify_songs_cleaned_without_trans.rds")
+        indices = paste0(BASE_DIR, "artifacts/training_indices_spotify_songs_cleaned_without_trans.rds"),
+        isTransformed = FALSE
     ),
     list(
         data = paste0(DIR_DATA_CLEANING, "spotify_songs_cleaned_with_trans.RData"),
-        indices = paste0(BASE_DIR, "artifacts/training_indices_spotify_songs_cleaned_with_trans.rds")
+        indices = paste0(BASE_DIR, "artifacts/training_indices_spotify_songs_cleaned_with_trans.rds"),
+        isTransformed = TRUE
     ),
     list(
         data = paste0(DIR_DATA_CLEANING, "spotify_songs_cleaned_with_trans_optima.RData"),
-        indices = paste0(BASE_DIR, "artifacts/training_indices_spotify_songs_cleaned_with_trans_optima.rds")
+        indices = paste0(BASE_DIR, "artifacts/training_indices_spotify_songs_cleaned_with_trans_optima.rds"),
+        isTransformed = TRUE
     )
 )
 
-test_models <- function(dataset, indices_file) {
+test_models <- function(dataset, indices_file, isTransformed) {
     
     data <- get(load(dataset$data))
     training_indices <- readRDS(indices_file)
@@ -38,16 +42,27 @@ test_models <- function(dataset, indices_file) {
         
         # Check if model file exists
         if (file.exists(model_file)) {
+
             model <- readRDS(model_file)
 
             # Predict on test data
             predictions <- predict(model, newdata = test_data)
 
+            # Backtransformation
+            if (isTransformed) {
+                backtrans <- backtransformation(predictions, test_data$streams)
+                pred <- backtrans$pred
+                actual <- backtrans$actual
+            } else {
+                pred <- predictions
+                actual <- test_data$streams
+            }
+
             # Calculate evaluation metrics
-            rmse <- sqrt(mean((test_data$streams - predictions)^2))
-            mae <- mean(abs(test_data$streams - predictions))
-            mape <- mean(abs((test_data$streams - predictions) / test_data$streams)) * 100
-            mse <- mean((test_data$streams - predictions)^2)
+            rmse <- sqrt(mean((actual - pred)^2))
+            mae <- mean(abs(actual - pred))
+            mape <- mean(abs((actual - pred) / actual)) * 100
+            mse <- mean((actual - pred)^2)
             r_squared <- summary(model)$r.squared
 
             n <- nrow(test_data) # Number of observations
@@ -58,15 +73,43 @@ test_models <- function(dataset, indices_file) {
             aic_value <- AIC(model)
             bic_value <- BIC(model)
 
+            observed_vs_predicted(sprintf("%simg/%s_%s", BASE_DIR, model_type, basename(gsub(".RData", "", dataset$data))), pred, actual)
+
             # Print evaluation metrics
             cat(sprintf("Model: %s\nRMSE: %f\nMAE: %f\nMAPE: %f\nMSE: %f\nR-squared: %f\nAdjusted R-squared: %f\nF-statistic: %f\nAIC: %f\nBIC: %f\n\n",
                         model_type, rmse, mae, mape, mse, r_squared, adj_r_squared, f_statistic, aic_value, bic_value))
+        
+            saveRDS(list(regression = predictions, mae = mae, mse = mse, rmse = rmse), sprintf("%sartifacts/results_%s_%s.rds", BASE_DIR, model_type, basename(gsub(".RData", "", dataset$data))))
         }
     }
 }
 
+# Rücktransformation der mittels Logarithmus transformierten Vorhersagen und akteulle Werte
+backtransformation <- function(pred, actual){
+  log_pred <- pred 
+  log_actual <- actual  
+  pred <- exp(log_pred)
+  actual <- exp(log_actual)
+  return (list(pred = pred, actual = actual))
+}
+
+# Plot observed vs predicted values
+observed_vs_predicted <- function(path_name, actual, pred) {
+  full_filename <- paste0(path_name, "_regression_observed_vs_predicted.png")
+  png(file = full_filename, width = 800, height = 600)
+  plot <- ggplot(data = NULL, aes(x = actual, y = pred)) +
+    geom_text(label="★", color="orange", size=3) +  # Fügt die Punkte hinzu
+    geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "blue") + # Fügt eine y=x Linie hinzu
+    labs(x = "Observed", y = "Predicted", title = "Observed vs Predicted Plot") +
+    theme_minimal()
+  print(plot)
+  # Schließen der PNG-Aufzeichnung
+  dev.off()
+}
+
+
 # Test each dataset
 for (dataset in datasets) {
     cat("Testing dataset:", dataset$data, "\n")
-    test_models(dataset, dataset$indices)
+    test_models(dataset, dataset$indices, dataset$isTransformed)
 }
